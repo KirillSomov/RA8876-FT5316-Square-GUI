d90e38632f0506a713929d45d152548f3c01a5eb
Kirill <kirichrus@gmail.com>
2022-09-25 04:22:59 +0300

    test idle

--- lib/myGUI/myGUI.c
+++ lib/myGUI/myGUI.c
@@ -34,7 +34,7 @@ static void page_0_init(void)
   SGUI_createLabel(0, 100, 100, 150, 150, 0, 0, 10, Red, Blue, 0, 0, 0, 0, 0);
   SGUI_createButton(0, 400, 200, 500, 300, 0, 0, 2, Yellow, Magenta, 0, 0, 0, 0, 0, 1000, btnAct1);
   SGUI_createButton(0, 600, 200, 700, 300, 10, 10, 5, Magenta, Yellow, 0, 0, 0, 0, 0, 5000, btnAct2);
-  SGUI_createButton(0, 850, 50, 900, 100, 0, 0, 2, Red, Grey, 0, 0, 0, 0, 0, 1000, btnGoToPage1);
+  SGUI_createButton(0, 850, 50, 900, 100, 0, 0, 2, Red, Grey, 0, 0, 0, 0, 0, 100, btnGoToPage1);
   SGUI_createPicture(0, &image_deb_8bpp, 50, 50, 400, 422, 8, 8, 4, 0x4BC6);
   SGUI_drawFrame(800, 400, 900, 500,
                  5, 5,
@@ -51,7 +51,7 @@ static void page_1_init(void)
   SGUI_createLabel(1, 150, 150, 200, 200, 0, 0, 10, Blue, Red, 0, 0, 0, 0, 0);
   SGUI_createButton(1, 450, 250, 550, 350, 0, 0, 2, Black, Green, 0, 0, 0, 0, 0, 1000, btnAct1);
   SGUI_createButton(1, 650, 250, 750, 350, 0, 0, 2, Green, Grey, 0, 0, 0, 0, 0, 5000, btnAct2);
-  SGUI_createButton(1, 850, 50, 900, 100, 0, 0, 2, Red, Grey, 0, 0, 0, 0, 0, 1000, btnGoToPage0);
+  SGUI_createButton(1, 850, 50, 900, 100, 0, 0, 2, Red, Grey, 0, 0, 0, 0, 0, 100, btnGoToPage0);
   SGUI_drawPage(1);
 }
 
